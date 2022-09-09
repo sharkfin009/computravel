5c4136ca3d61a1252569b2bcd61ebfe67321acee
ben amato <ben.amato@gmail.com>
2022-09-09 09:26:52 +0200

    about to refeactor suggest data

--- components/Search/AutoSuggestOverlay.vue
+++ components/Search/AutoSuggestOverlay.vue
@@ -12,11 +12,34 @@
     "
     v-if="suggestStore.showSuggestions"
   >
-    <div
-      class="rounded-lala bg-gray-200 p-10 opacity-100 grid grid-cols-[3fr,1fr]"
-    >
-      <div class="hidden md:block w-full">
-        <div class="font-medium pl-20 pb-2">packages:</div>
+    <div class="rounded-lala bg-gray-200 p-10 opacity-100 flex flex-col gap-5">
+      <div class="font-semibold ml-20">destinations:</div>
+      <div class="ml-20">
+        <div class="flex gap-2">
+          <div
+            v-for="(item, index) in suggestStore.destinationSuggestions"
+            :key="index"
+            class="
+              flex
+              items-center
+              bg-slate-50
+              rounded-xl
+              pl-1
+              pr-4
+              py-1
+              text-lime-700
+              hover:bg-lime-500 hover:text-lime-200
+              cursor-pointer
+            "
+            @mousedown="suggestStore.searchDestination(item.name)"
+          >
+            <IconSearch class="h-4 text-black" />
+            <div class="">{{ item.name }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="font-semibold ml-20">packages:</div>
+      <div class="w-full">
         <div
           v-for="(suggestion, index) in suggestStore.packageSuggestions"
           :class="{
@@ -27,7 +50,7 @@
           class=""
         >
           <div
-            class="block md:grid md:grid-cols-[auto,200px] rounded-full"
+            class="grid grid-cols-[auto,200px] rounded-full"
             :class="{
               '!bg-lime-500 !bg-opacity-80 !text-white':
                 index === hoveredSuggestion - 1,
@@ -48,7 +71,7 @@
                 "
               >
                 <div
-                  class="hidden lg:block font-semibold mb-3 text-lime-700"
+                  class="font-semibold mb-3 text-lime-700"
                   :class="{
                     ' !text-lime-200': index === hoveredSuggestion - 1,
                     ' text-lime-200': index === selectedSuggestion,
@@ -57,15 +80,14 @@
                   {{ suggestion.titleShort }}
                 </div>
 
-                <div class="hidden lg:block font-light">
+                <div class="font-light">
                   {{ suggestion.description }}
                 </div>
               </div>
             </div>
             <div
               class="
-                hidden
-                lg:flex
+                flex
                 text-lime-700
                 items-center
                 justify-end
@@ -79,52 +101,6 @@
             >
               {{ suggestion.destination }}
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="hidden md:block pl-3">
-        <div class="pb-2 font-medium">destinations:</div>
-        <div class="grid ld:grid-cols-2 gap-2">
-          <div
-            v-for="(item, index) in suggestStore.destinationSuggestions"
-            :key="index"
-            class="
-              flex
-              items-center
-              bg-slate-50
-              rounded-full
-              p-1
-              text-lime-700
-              hover:bg-lime-500 hover:text-lime-200
-              cursor-pointer
-            "
-            @mousedown="suggestStore.searchDestination(item.name)"
-          >
-            <IconSearch class="h-4 text-black" />
-            <div class="">{{ item.name }}</div>
-          </div>
-        </div>
-      </div>
-      <!-- mobile: -->
-      <div class="block md:hidden p-2 h-[250px] overflow-auto">
-        <div class="mb-2">related searches:</div>
-        <div class="grid gap-2">
-          <div
-            v-for="(item, index) in suggestStore.destinationSuggestions"
-            :key="index"
-            class="
-              flex
-              items-center
-              bg-slate-50
-              rounded-full
-              p-1
-              hover:bg-lime-500 hover:text-lime-200
-            "
-            @mousedown="searchDestination(item.name)"
-          >
-            <IconSearch class="h-4" />
-            <div class="">{{ item.name }}</div>
           </div>
         </div>
       </div>
