d51d4f79c8b2060fa92e9b29c57dd050d6fa5e5c
ben amato <ben.amato@gmail.com>
2022-11-02 16:37:17 +0200

    doing it

--- pages/cookie-policy.vue
+++ pages/cookie-policy.vue
@@ -1,231 +1,24 @@
 <template>
-  <div class="p-10">
-    <h3 class="font-titillium text-4xl mb-10 font-bold">Cookie Policy</h3>
-    <div class="kopa-content">
-      <p><strong>Introduction</strong></p>
-      <p>
-        COMPUTRAVEL CC may use cookies, web beacons, tracking pixels, and other
-        tracking technologies when you visit our website
-        https://computravel.co.za/home, including any other media form, media
-        channel, mobile website, or mobile application related or connected
-        thereto (collectively, the “Site”) to help customiSe the Site and
-        improve your experience.
-      </p>
-      <p>
-        We reserve the right to make changes to this Cookie Policy at any time
-        and for any reason. We will alert you about any changes by updating the
-        “Last Updated” date of this Cookie Policy. Any changes or modifications
-        will be effective immediately upon posting the updated Cookie Policy on
-        the Site, and you waive the right to receive specific notice of each
-        such change or modification.
-      </p>
-      <p>
-        You are encouraged to periodically review this Cookie Policy to stay
-        informed of updates. You will be deemed to have been made aware of, will
-        be subject to, and will be deemed to have accepted the changes in any
-        revised Cookie Policy by your continued use of the Site after the date
-        such revised Cookie Policy is posted.
-      </p>
-
-      <p><strong>Use of Cookies</strong></p>
-      <p>
-        A “cookie” is a string of information which assigns you a unique
-        identifier that we store on your computer. Your browser then provides
-        that unique identifier to use each time you submit a query to the Site.
-        We use cookies on the Site to, among other things, keep track of
-        services you have used, record registration information, record your
-        user preferences, keep you logged into the Site, facilitate purchase
-        procedures, and track the pages you visit. Cookies help us understand
-        how the Site is being used and improve your user experience.
-      </p>
-
-      <p><strong>Types of cookies</strong></p>
-      <p>
-        The following types of cookies may be used when you visit the
-        <a href="<?= BASE_URL ?>home" title="Computravel site link">Site</a>:
-      </p>
-
-      <div class="kopa-tab style-06">
-        <ul class="tab-navigation">
-          <li class="active"><a href="#">Advertising</a></li>
-          <li><a href="#">Analytics</a></li>
-          <li><a href="#">Our</a></li>
-          <li><a href="#">Personalisation</a></li>
-          <li><a href="#">Security</a></li>
-          <li><a href="#">Site Management</a></li>
-          <li><a href="#">Third-Party</a></li>
-        </ul>
-        <!-- tab-navigation -->
-
-        <div class="tab-navigation-mobile sj-dropdown">
-          <span class="sj-dropdown-btn">Advertising Cookies</span>
-          <ul class="sj-dropdown-content">
-            <li><a href="#">Advertising Cookies</a></li>
-            <li><a href="#">Analytics Cookies</a></li>
-            <li><a href="#">Our Cookies</a></li>
-            <li><a href="#">Personalisation Cookies</a></li>
-            <li><a href="#">Security Cookies</a></li>
-            <li><a href="#">Site Management Cookies</a></li>
-            <li><a href="#">Third-Party Cookies</a></li>
-          </ul>
-        </div>
-        <!-- tab-navigation-mobile -->
-
-        <div class="tab-content">
-          <div class="tab-panel active">
-            <p>
-              Advertising cookies are placed on your computer by advertisers and
-              ad servers in order to display advertisements that are most likely
-              to be of interest to you. These cookies allow advertisers and ad
-              servers to gather information about your visits to the Site and
-              other websites, alternate the ads sent to a specific computer, and
-              track how often an ad has been viewed and by whom. These cookies
-              are linked to a computer and do not gather any personal
-              information about you.
-            </p>
-          </div>
-          <!-- tab-panel -->
-
-          <div class="tab-panel">
-            <p>
-              Analytics cookies monitor how users reached the Site, and how they
-              interact with and move around once on the Site. These cookies let
-              us know what features on the Site are working the best and what
-              features on the Site can be improved.
-            </p>
-          </div>
-          <!-- tab-panel -->
-
-          <div class="tab-panel">
-            <p>
-              Our cookies are “first-party cookies”, and can be either permanent
-              or temporary. These are necessary cookies, without which the Site
-              won’t work properly or be able to provide certain features and
-              functionalities. Some of these may be manually disabled in your
-              browser, but may affect the functionality of the Site.
-            </p>
-          </div>
-          <!-- tab-panel -->
-
-          <div class="tab-panel">
-            <p>
-              Personalisation cookies are used to recognise repeat visitors to
-              the Site. We use these cookies to record your browsing history,
-              the pages you have visited, and your settings and preferences each
-              time you visit the Site.
-            </p>
-          </div>
-          <!-- tab-panel -->
-
-          <div class="tab-panel">
-            <p>
-              Security cookies help identify and prevent security risks. We use
-              these cookies to authenticate users and protect user data from
-              unauthorised parties.
-            </p>
-          </div>
-          <!-- tab-panel -->
-          <div class="tab-panel">
-            <p>
-              Site management cookies are used to maintain your identity or
-              session on the Site so that you are not logged off unexpectedly,
-              and any information you enter is retained from page to page. These
-              cookies cannot be turned off individually, but you can disable all
-              cookies in your browser.
-            </p>
-          </div>
-          <!-- tab-panel -->
-          <div class="tab-panel">
-            <p>
-              Third-party cookies may be place on your computer when you visit
-              the Site by companies that run certain services we offer. These
-              cookies allow the third parties to gather and track certain
-              information about you. These cookies can be manually disabled in
-              your browser.
-            </p>
-          </div>
-          <!-- tab-panel -->
-        </div>
-        <!-- tab-content -->
-      </div>
-
-      <br />
-      <p><strong>Control of cookies</strong></p>
-      <p>
-        Most browsers are set to accept cookies by default. However, you can
-        remove or reject cookies in your browser’s settings. Please be aware
-        that such action could affect the availability and functionality of the
-        Site.
-      </p>
-
-      /*
-      <p>
-        For more information on how to control cookies, check your browser or
-        device’s settings for how you can control or reject cookies, or visit
-        the following links:
-      </p>
-      <ul>
-        <li>Apple Safari</li>
-        <li>Google Chrome</li>
-        <li>Microsoft Edge</li>
-        <li>Microsoft Internet Explorer</li>
-        <li>Mozilla Firefox</li>
-        <li>Opera</li>
-        <li>Android (Chrome)</li>
-        <li>Iphone or Ipad (Chrome)</li>
-        <li>Iphone or Ipad (Safari)</li>
-        <li>Apple Safari</li>
-      </ul>
-      <p>
-        In addition, you may opt-out of some third-party cookies through the
-        Network Advertising Initiative’s Opt-Out Tool.
-      </p>
-
-      <p><strong>Other tracking technologies</strong></p>
-      <p>
-        In addition to cookies, we may use web beacons, pixel tags, and other
-        tracking technologies on the Site to help customize the Site and improve
-        your experience. A “web beacon” or “pixel tag” is tiny object or image
-        embedded in a web page or email. They are used to track the number of
-        users who have visited particular pages and viewed emails, and acquire
-        other statistical data. They collect only a limited set of data, such as
-        a cookie number, time and date of page or email view, and a description
-        of the page or email on which they reside. Web beacons and pixel tags
-        cannot be declined. However, you can limit their use by controlling the
-        cookies that interact with them.
-      </p>
-
-      <p><strong>Privacy policy</strong></p>
-      <p>
-        For more information about how we use information collected by cookies
-        and other tracking technologies, please refer to our
-        <a href="<?= BASE_URL ?>privacy-policy" title="Privacy policy page"
-          >Privacy Policy</a
-        >
-        posted on the Site. This Cookie Policy is part of and is incorporated
-        into our Privacy Policy. By using the Site, you agree to be bound by
-        this Cookie Policy and our Privacy Policy.
-      </p>
-
-      <p><strong>Contact us</strong></p>
-      <p>
-        If you have questions or comments about this Cookie Policy, please
-        contact us at:
-      </p>
-      <p><strong>Computravel cc</strong></p>
-      <p>
-        59 Waterloo Road<br />
-        Wynberg<br />
-        Cape Town<br />
-        South Africa
-      </p>
-      <p>+27 21 762 0511</p>
-      <p>+27 84 674 0246</p>
-    </div>
+  <div v-if="data">
+    <div
+      class="policies p-2 md:p-20"
+      v-html="data.data.cookiePolicy.data.attributes.content"
+    />
   </div>
 </template>
 <script setup>
 definePageMeta({
   layout: "home",
 });
+
+const { data, error } = useGraph(`
+query{cookiePolicy{
+  data{attributes{
+    content
+  }}
+}}
+`);
 </script>
+
+<style scoped>
+</style>
--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,140 +1,24 @@
 <template>
-  <div class="p-10">
-    <h3 class="font-titillium text-4xl font-bold mb-10">Privacy Policy</h3>
-
-    <div class="kopa-content">
-      <p>
-        <strong>Computravel.co.za </strong>respects each individual&rsquo;s
-        right to personal privacy. We will collect and use information through
-        our Web site only in the ways disclosed in this statement. This
-        statement applies solely to information collected at
-        <strong>www.computravel.co.za</strong> website.
-      </p>
-      <br />
-      <h4 class="font-titillium text-2xl font-bold mb-3">
-        Information Collection
-      </h4>
-      <br />
-      <p>
-        <strong>Computravel.co.za</strong> collects information through our
-        Website at several points. <br />
-        We collect the following information about primary visitors: Name,
-        address, email, phone, passport details, proposed travelling dates,
-        number of passengers and destination when an enquiry booking is made
-        with us. <br />
-        Through a secure login we retrieve holiday package information from Tour
-        Operators: destinations, inclusions, exclusions, price, validity and
-        conditions.
-      </p>
-      <br />
-      <p>
-        <strong>Computravel.co.za</strong> does not actively market to children,
-        and we never knowingly ask a child under 13 to divulge personal
-        information. <br />
-        <br />
-        We employ cookies. A cookie is a small text file that our Web server
-        places on a user&rsquo;s computer hard drive to be a unique identifier.
-        Cookies enable <strong>Computravel.co.za</strong> to track usage
-        patterns and deliver customized content to users. Our cookies do not
-        collect personally identifiable information. For more information about
-        our use of cookies, please refer to our
-        <a href="<?= BASE_URL ?>cookie-policy" title="Cookie policy page"
-          >Cookie Policy</a
-        >
-        posted on the Computravel.co.za site.
-      </p>
-      <br />
-    </div>
-
-    <div class="kopa-tab style-06">
-      <ul class="tab-navigation">
-        <li class="active"><a href="#">Information Usage</a></li>
-        <li><a href="#">Access to Information</a></li>
-        <li><a href="#">Problem Resolution</a></li>
-        <li><a href="#">Data Storage and Security</a></li>
-      </ul>
-      <!-- tab-navigation -->
-
-      <div class="tab-navigation-mobile sj-dropdown">
-        <span class="sj-dropdown-btn">Information Usage</span>
-        <ul class="sj-dropdown-content">
-          <li><a href="#">Information Usage</a></li>
-          <li><a href="#">Access to Information</a></li>
-          <li><a href="#">Problem Resolution</a></li>
-          <li><a href="#">Data Storage and Security</a></li>
-        </ul>
-      </div>
-      <!-- tab-navigation-mobile -->
-
-      <div class="tab-content">
-        <div class="tab-panel active">
-          <p>
-            The personal information that we gather is used solely for
-            identification and booking enquiry purposes and for managing online
-            profiles. <strong>Computravel.co.za</strong> does not sell or
-            otherwise exploit personal information.
-          </p>
-          <p>
-            <strong>Computravel.co.za</strong> may gather generic and
-            non-identifying information for marketing purposes for use by
-            <strong>Computravel.co.za</strong> and, in some instances, by our
-            registered travel experts. Information about your activity on the
-            site may be automatically collected when you log in and is used for
-            the sole purpose of improving the Web site. <br />
-            We might offer links to other Web sites. Please note: When you click
-            on links to other Web sites, we encourage you to read their privacy
-            policies. Their standards may differ from ours.
-          </p>
-          <p>
-            If our policy on information collection or uses changes, will advise
-            you by updating this policy on our web site.
-          </p>
-        </div>
-        <!-- tab-panel -->
-
-        <div class="tab-panel">
-          <p>
-            <strong>Computravel.co.za</strong> strives to maintain the accuracy
-            of our information. Registered users may access their own personal
-            information and contact us about inaccuracies they may find by
-            contacting us at the email address below. Users may also delete
-            their information from our database by contacting us at the same
-            address. In both cases, please contact us using the email address
-            given to you when registering with computravel.co.za
-          </p>
-        </div>
-        <!-- tab-panel -->
-
-        <div class="tab-panel">
-          <p>
-            In the event of any problems users may contact
-            <strong>Computravel.co.za</strong> by the email address below. We
-            are committed to resolving disputes within 3 working days.
-          </p>
-        </div>
-        <!-- tab-panel -->
-
-        <div class="tab-panel">
-          <p>
-            Personal information submitted by users is stored on a dedicated
-            <strong>Computravel.co.za </strong>server within a secure
-            datacentre. Computravel.co.za does not store financial information
-            such as credit card numbers and adheres to strict protocols
-            restricting the access of users personal information and contact
-            details. Users personal information is accessed only by
-            <strong>Computravel.co.za</strong> and by the assigned travel expert
-            registered with <strong>Computravel.co.za</strong>
-          </p>
-        </div>
-        <!-- tab-panel -->
-      </div>
-      <!-- tab-content -->
-    </div>
-    <!-- kopa-tab -->
+  <div v-if="data">
+    <div
+      class="policies p-2 md:p-20"
+      v-html="data.data.privacyPolicy.data.attributes.content"
+    />
   </div>
 </template>
 <script setup>
 definePageMeta({
   layout: "home",
 });
+
+const { data, error } = useGraph(`
+query{privacyPolicy{
+  data{attributes{
+    content
+  }}
+}}
+`);
 </script>
+
+<style scoped>
+</style>
